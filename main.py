5066c1ea4a55a79ad67f95b79665e219079a3c4e
TelisK <tkoutsogiannakis@hotmail.com>
2025-08-13 01:10:31 +0300

    working code basic

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Path
+import sqlite3
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'name' : 'first data'}
+
+@app.get('/{date}')
+def get_data(date : str):
+    conn = sqlite3.connect('income_outcome.db')
+    c = conn.cursor()
+    c.execute("SELECT income.date, income.income_cash, income.income_pos, income.income, outcome.outcome FROM income LEFT JOIN outcome ON income.date = outcome.date WHERE income.date = ?", (date,))
+    rows = c.fetchall()
+    conn.close()
+    return {'date': rows}
