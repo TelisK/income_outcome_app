ca60ba5024bb7821e303cceb2aca44ed4fb87e5d
TelisK <tkoutsogiannakis@hotmail.com>
2025-08-13 01:12:37 +0300

    code with for loop to check the date and return a message not found

--- main.py
+++ main.py
@@ -13,5 +13,9 @@ def get_data(date : str):
     c = conn.cursor()
     c.execute("SELECT income.date, income.income_cash, income.income_pos, income.income, outcome.outcome FROM income LEFT JOIN outcome ON income.date = outcome.date WHERE income.date = ?", (date,))
     rows = c.fetchall()
+    for row in rows:
+        if row == date:
+            return {'date': rows}
+        else:
+            return {'data': 'Not Found'}
     conn.close()
-    return {'date': rows}
