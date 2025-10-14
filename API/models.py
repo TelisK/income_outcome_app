691f69d2ee13c21b432b5fbee23b859f961ca5f3
TelisK <tkoutsogiannakis@hotmail.com>
2025-10-15 00:29:05 +0300

    complete create user endpoint and hash password with bcrypt. Also made new requirements.txt file

--- API/models.py
+++ API/models.py
@@ -1,5 +1,17 @@
 from database import Base
-from sqlalchemy import ForeignKey, Column, Float, Date, Integer
+from sqlalchemy import ForeignKey, Column, Float, Date, Integer, String, Boolean, Enum # used to give only two roles for user.
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    active = Column(Boolean, default=True)
+    role = Column(Enum('admin', 'user', name='user_roles'), default='user')
 
 class Income(Base):
     __tablename__ = 'income'
@@ -12,6 +24,7 @@ class Income(Base):
 
 class Expenses(Base):
     __tablename__ = 'expenses'
-    id = Column(Integer, primary_key=True, index=True)
-    date =  Column(Date, ForeignKey('income.date'), unique=True)
+    #id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, index=True)
+    date =  Column(Date, ForeignKey('income.date'), primary_key=True, unique=True)
     expenses = Column(Float)
